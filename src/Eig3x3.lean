a345ba81ebf6d666d06da8c8ba00fd9f963b8388
Israel Flores-Arbolay <israelflores8789@gmail.com>
2026-08-23 21:07:18 +0000

    initialize Lean project structure and basic github workflow

---
+++ src/Eig3x3.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Eig3x3` library.
+-- Import modules here that should be built as part of the library.
+import Eig3x3.Basic
---
+++ src/Main.lean
@@ -0,0 +1,4 @@
+import Eig3x3
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
