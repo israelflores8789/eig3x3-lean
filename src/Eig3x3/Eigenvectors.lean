b107157a31db786fd3f8091a7919b408d89d4b45
Israel Flores-Arbolay <israelflores8789@gmail.com>
2026-08-27 00:04:30 +0000

    adding copyright headers

--- src/Eig3x3/Eigenvectors.lean
+++ src/Eig3x3/Eigenvectors.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Israel Flores-Arbolay. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Israel Flores-Arbolay
+-/
 module
 
 import all Eig3x3.Basic
